971dcb08608fc2f2dedaa0c22c145bad8f95a2cd
merkost <konstantinlikes@gmail.com>
2023-08-10 11:22:21 +1000

    GreetingScreen implemented

---
+++ app/src/main/java/com/merkost/suby/presentation/GreetingScreen.kt
@@ -0,0 +1,105 @@
+package com.merkost.suby.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.merkost.suby.R
+import com.merkost.suby.ui.theme.SubyTheme
+
+@Composable
+fun GreetingScreen(onContinueClick: () -> Unit) {
+    val systemUiController = rememberSystemUiController()
+    val isDarkTheme = isSystemInDarkTheme()
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            Color.Transparent,
+            darkIcons = !isDarkTheme
+        )
+    }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .windowInsetsPadding(WindowInsets.systemBars)
+                .padding(PaddingValues(32.dp))
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(200.dp)
+                    .weight(1f),
+                painter = painterResource(id = if (isDarkTheme) R.drawable.suby_logo_white else R.drawable.suby_logo_black),
+                contentDescription = "logo"
+            )
+
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Suby:",
+                    style = MaterialTheme.typography.displayMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Text(
+                    text = "Manage\nSubscriptions",
+                    style = MaterialTheme.typography.displaySmall.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+            ) {
+                FilledTonalButton(
+                    modifier = Modifier.align(Alignment.CenterStart),
+                    onClick = onContinueClick
+                ) {
+                    Text(
+                        text = "Continue",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GreetingScreenPreview() {
+    SubyTheme {
+        GreetingScreen {}
+    }
+}
