637baafaa104c3f1efe2385631bc34f947a68ec3
merkost <konstantinlikes@gmail.com>
2023-08-12 11:22:37 +1000

    Added Destinations

--- app/src/main/java/com/merkost/suby/utils/Destinations.kt
+++ app/src/main/java/com/merkost/suby/utils/Destinations.kt
@@ -4,10 +4,15 @@ package com.merkost.suby.utils
  * Destinations used in the [SubyApp].
  */
 object Destinations {
+    const val CURRENCY_PICK = "currency_pick"
     const val GREETING = "greeting"
     const val ONBOARDING = "onboarding"
 
     const val MAIN_SCREEN = "main"
     const val NEW_SUBSCRIPTION = "new"
 
+}
+
+object Arguments {
+    const val CURRENCY = "currency_arg"
 }
