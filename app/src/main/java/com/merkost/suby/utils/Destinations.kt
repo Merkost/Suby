87a26766adeaaff528de5a59222df7932078206e
merkost <konstantinlikes@gmail.com>
2023-08-14 11:09:07 +1000

    Destinations refactored

--- app/src/main/java/com/merkost/suby/utils/Destinations.kt
+++ app/src/main/java/com/merkost/suby/utils/Destinations.kt
@@ -4,13 +4,17 @@ package com.merkost.suby.utils
  * Destinations used in the [SubyApp].
  */
 object Destinations {
-    const val CURRENCY_PICK = "currency_pick"
+
+
+    const val FEEDBACK = "feedback"
     const val GREETING = "greeting"
     const val ONBOARDING = "onboarding"
 
     const val MAIN_SCREEN = "main"
     const val NEW_SUBSCRIPTION = "new"
 
+    const val CURRENCY_PICK = "currency_pick"
+    const val MAIN_CURRENCY_PICK = "main_currency_pick"
 }
 
 object Arguments {
