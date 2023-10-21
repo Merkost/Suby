d51b3c8bb54d41dffcd37e68e477477f331b7680
merkost <konstantinlikes@gmail.com>
2023-10-21 16:59:11 +1100

    Versions upgraded

--- app/src/main/java/com/merkost/suby/ui/theme/Color.kt
+++ app/src/main/java/com/merkost/suby/ui/theme/Color.kt
@@ -10,5 +10,15 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Color.Companion.Orange: Color
-    get() = Color(0xFFFFA500)
+val Color.Companion.StatusOrange: Color
+    get() = Color(0xFFFB8C00)
+
+val Color.Companion.StatusGreen: Color
+    get() = Color(0xFF43A047)
+
+val Color.Companion.StatusRed: Color
+    get() = Color(0xFFE53935)
+
+val Color.Companion.StatusYellow: Color
+    get() = Color(0xFFFDD835)
+
