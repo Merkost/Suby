d2b18cff53fc9020423c29d2b721246e839a0fb4
merkost <konstantinlikes@gmail.com>
2024-01-07 22:09:38 +1100

    nextBillingDate function implemented in Period

--- app/src/main/java/com/merkost/suby/model/Period.kt
+++ app/src/main/java/com/merkost/suby/model/Period.kt
@@ -1,5 +1,9 @@
 package com.merkost.suby.model
 
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toKotlinLocalDate
+
 enum class Period(val periodName: String, val days: Long) {
     DAILY("Daily",1),
     WEEKLY("Weekly", 7),
@@ -7,8 +11,8 @@ enum class Period(val periodName: String, val days: Long) {
     MONTHLY("Monthly", 30),
     QUARTERLY("Quarterly", 90),
     SEMI_ANNUAL("Semi-Annually", 180),
-    ANNUAL("Annually", 365),
-    CUSTOM("Custom", 0);
+    ANNUAL("Annually", 365);
+//    CUSTOM("Custom", 0);
 
     fun nextMain(): Period {
         return when (this) {
@@ -16,5 +20,17 @@ enum class Period(val periodName: String, val days: Long) {
             WEEKLY -> MONTHLY
             else -> DAILY
         }
+    }
+
+    fun nextBillingDate(fromDate: LocalDate): LocalDate {
+        val fromDate = fromDate.toJavaLocalDate()
+        val res =  when (this) {
+            MONTHLY -> fromDate.plusMonths(1)
+            QUARTERLY -> fromDate.plusMonths(3)
+            SEMI_ANNUAL -> fromDate.plusMonths(6)
+            ANNUAL -> fromDate.plusYears(1)
+            else -> fromDate.plusDays(this.days.toLong())
+        }
+        return res.toKotlinLocalDate()
     }
 }
