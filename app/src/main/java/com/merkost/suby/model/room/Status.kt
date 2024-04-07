2168adacb5ca40b7f13113fd4a72e58f8d250760
merkost <konstantinlikes@gmail.com>
2024-04-07 10:08:18 +1000

    Added description to Status

--- app/src/main/java/com/merkost/suby/model/room/Status.kt
+++ app/src/main/java/com/merkost/suby/model/room/Status.kt
@@ -12,9 +12,34 @@ import com.merkost.suby.ui.theme.StatusOrange
 import com.merkost.suby.ui.theme.StatusRed
 import com.merkost.suby.ui.theme.StatusYellow
 
-enum class Status(val icon: ImageVector, val color: Color, val statusName: String) {
-    ACTIVE(Icons.Outlined.CheckCircleOutline, Color.StatusGreen, "Active"),
-    CANCELED(Icons.Outlined.Cancel, Color.StatusRed, "Canceled"),
-    EXPIRED(Icons.Outlined.DoNotDisturbOn, Color.StatusOrange, "Expired"),
-    TRIAL(Icons.Outlined.Timelapse, Color.StatusYellow, "Trial");
+enum class Status(
+    val icon: ImageVector,
+    val color: Color,
+    val statusName: String,
+    val description: String
+) {
+    ACTIVE(
+        Icons.Outlined.CheckCircleOutline,
+        Color.StatusGreen,
+        "Active",
+        "for subscriptions that are currently ongoing and for which you are regularly being charged. "
+    ),
+    CANCELED(
+        Icons.Outlined.Cancel,
+        Color.StatusRed,
+        "Canceled",
+        "for subscriptions that you have actively discontinued before their expiration date."
+    ),
+    EXPIRED(
+        Icons.Outlined.DoNotDisturbOn,
+        Color.StatusOrange,
+        "Expired",
+        "for subscriptions that have reached their end date and are no longer active, but haven't been formally canceled."
+    ),
+    TRIAL(
+        Icons.Outlined.Timelapse,
+        Color.StatusYellow,
+        "Trial",
+        "for subscriptions that are in their trial period."
+    );
 }
