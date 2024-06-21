40ffd21419cbe4984bdea535561be5e08902bca4
merkost <konstantinlikes@gmail.com>
2024-06-21 19:37:52 +1000

    Created custom AppColors

--- app/src/main/java/com/merkost/suby/ui/theme/Theme.kt
+++ app/src/main/java/com/merkost/suby/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.merkost.suby.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,11 +8,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -37,6 +36,12 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+val LocalAppColors = staticCompositionLocalOf { lightColors }
+
+val MaterialTheme.subyColors: AppColors
+    @Composable
+    get() = LocalAppColors.current
+
 @Composable
 fun SubyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -53,6 +58,11 @@ fun SubyTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
+    val colors = when {
+        darkTheme -> darkColors
+        else -> lightColors
+    }
 //    val view = LocalView.current
 //    if (!view.isInEditMode) {
 //        SideEffect {
@@ -62,9 +72,24 @@ fun SubyTheme(
 //        }
 //    }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(LocalAppColors provides colors) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
+
+val lightColors = AppColors(
+    textPlaceholderColor = Color.LightGray
+)
+
+val darkColors = AppColors(
+    textPlaceholderColor = Color.Gray
+)
+
+@Stable
+data class AppColors(
+    val textPlaceholderColor: Color,
+)
