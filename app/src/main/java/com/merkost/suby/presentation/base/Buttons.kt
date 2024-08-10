e0a1154b95af1ba2d38c6f12ccf85b1bcb9faaaf
merkost <konstantinlikes@gmail.com>
2024-08-10 13:51:48 +1000

    Save button improvements

--- app/src/main/java/com/merkost/suby/presentation/base/Buttons.kt
+++ app/src/main/java/com/merkost/suby/presentation/base/Buttons.kt
@@ -1,10 +1,12 @@
 package com.merkost.suby.presentation.base
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -13,14 +15,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -46,19 +49,31 @@ fun SubyButton(
 
 @Composable
 fun SaveButton(modifier: Modifier = Modifier, enabled: Boolean, onClick: () -> Unit) {
-    FilledTonalButton(modifier = modifier, onClick = onClick, enabled = enabled) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 12.dp)
+    ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
-            Image(
-                modifier = Modifier.size(32.dp),
-                painter = painterResource(id = R.drawable.suby_logo_white),
-                contentDescription = ""
-            )
-            Spacer(Modifier.size(4.dp))
+            AnimatedContent(enabled) {
+                if (it) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Image(
+                            modifier = Modifier.size(32.dp),
+                            painter = painterResource(id = R.drawable.suby_logo_black),
+                            contentDescription = "",
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimary)
+                        )
+                        Spacer(Modifier.size(4.dp))
+                    }
+                }
+            }
             Text(
-                text = "Save",
+                text = stringResource(R.string.btn_save),
                 style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
             )
         }
