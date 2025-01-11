954b3245540bcacba8b2d2bfc49e1cd2d87888fd
merkost <konstantinlikes@gmail.com>
2025-01-12 10:41:55 +1100

    release: version 0.1.11 (12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.merkost.suby"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.1.10"
+        versionCode = 12
+        versionName = "0.1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
